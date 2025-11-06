53142a42cb54388150b57e933c0cb5e135ad0563
Arjit Rawat <180564763+arjitrawat15@users.noreply.github.com>
2025-11-06 23:35:32 +0530

    Update TwoSum.cpp
    Implement brute force approach for Two Sum problem.

--- Arrays/TwoSum.cpp
+++ Arrays/TwoSum.cpp
@@ -1,1 +1,24 @@
-
+# Approach 1- 
+#include<iostream>
+using namespace std;
+int main()
+{
+  int n;
+  cin>>n;
+  vector<int>arr(n);
+  for(int i=0;i<n;++)
+  {
+    cin>>arr[i];
+  }
+  int target;
+  cin>>target;
+  for(int i=0;i<n;i++)
+  {
+    for(int j=i+1;j<n;j++)
+{
+if(nums[i]+nums[j]==target)
+{
+return true; //exists
+}
+}
+}
