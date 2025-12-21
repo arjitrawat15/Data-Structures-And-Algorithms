b241b7cac36b26c4378884eed31669e4725ceadb
Arjit Rawat <180564763+arjitrawat15@users.noreply.github.com>
2025-12-21 22:53:51 +0530

    Add files via upload

---
+++ Trees/Level-Order-Traversal.cpp
@@ -0,0 +1,83 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct node
+{
+    int data;
+    node* right;
+    node* left;
+
+    node(int val)
+    {
+        data=val;
+        left=right=NULL;
+    }
+};
+node* insert(node* root,int val)
+{
+    if(root==NULL)
+    {
+        return new node(val);
+    }
+    queue<node*> q;
+    q.push(root);
+    while(!q.empty())
+    {
+        node* curr=q.front();
+        q.pop();
+        if(curr->left==NULL)
+        {
+            curr->left=new node(val);
+            break;
+        }
+        else
+        {
+            q.push(curr->left);
+        }
+        if(curr->right==NULL)
+        {
+            curr->right=new node(val);
+            break;
+        }
+        else
+        {
+            q.push(curr->right);
+        }
+    }
+    return root;
+}
+void levelorder(node* root)
+{
+    if(root==NULL) return;
+    queue<node*>q;
+    q.push(root);
+    while(!q.empty())
+    {
+        node* curr=q.front();
+        q.pop();
+        cout<<curr->data<<" ";
+        if(curr->left!=NULL)
+        {
+            q.push(curr->left);
+        }
+        if(curr->right!=NULL)
+        {
+            q.push(curr->right);
+        }
+    }
+}
+int main()
+{
+    node* root=NULL;
+    int n,val;
+    cout<<"Enter the number of nodes: ";
+    cin>>n;
+    cout<<"Enter the nodes: ";
+    for(int i=0;i<n;i++)
+    {
+        cin>>val;
+        root=insert(root,val);
+    }
+    cout<<"Level Order Traversal is: ";
+    levelorder(root);
+    return 0;
+}
