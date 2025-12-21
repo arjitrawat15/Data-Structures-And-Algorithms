4c00aa1b2f2b7ca94e9e522c689a4578f9c6f74c
Arjit Rawat <180564763+arjitrawat15@users.noreply.github.com>
2025-12-21 22:52:28 +0530

    Add files via upload

---
+++ Trees/Postfix-Traversal.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct node
+{
+    int data;
+    node* left;
+    node* right;
+
+    node(int val)
+    {
+        data=val;
+        left=right=NULL;
+    }
+};
+node* insert(node* root,int val)
+{
+    if(root==NULL)
+    {
+        return new node(val);
+    }
+    queue<node*>q;
+    q.push(root);
+    while(!q.empty())
+    {
+        node* curr=q.front();
+        q.pop();
+        if(curr->left==NULL)
+        {
+            curr->left=new node(val);
+            break;
+        }
+        else
+        {
+            q.push(curr->left);
+        }
+        if(curr->right==NULL)
+        {
+            curr->right=new node(val);
+            break;
+        }
+        else
+        {
+            q.push(curr->right);
+        }
+    }
+    return root;
+}
+void postfix(node* root)
+{
+    if(root==NULL)
+    {
+        return;
+    }
+    postfix(root->left);
+    postfix(root->right);
+    cout<<root->data<<" ";
+}
+int main()
+{
+    node* root=NULL;
+    int n,val;
+    cout<<"Enter the number of nodes: ";
+    cin>>n;
+    cout<<"Enter the nodes: ";
+    for(int i=0;i<n;i++)
+    {
+        cin>>val;
+        root=insert(root,val);
+    }
+    cout<<"Postfix Traversal is: ";
+    postfix(root);
+    return 0;
+}
