331efba21a7febcaf3efb071a13bae7b4a2c6d00
Arjit Rawat <180564763+arjitrawat15@users.noreply.github.com>
2025-12-22 19:49:31 +0530

    Add files via upload

---
+++ Trees/Iterative-Inorder.cpp
@@ -0,0 +1,88 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct node
+{
+    int data;
+    node* left;
+    node* right;
+    
+    node(int val)
+    {
+        data=val;
+        left=right=NULL;
+    }
+};
+node* insert(node* root,int val)
+{
+    if(root==NULL)
+    {
+        return new node(val);
+    }
+    queue<node*>q;
+    q.push(root);
+    while(!q.empty())
+    {
+        node* curr=q.front();
+        q.pop();
+        if(curr->left==NULL)
+        {
+            curr->left=new node(val);
+            break;
+        }
+        else
+        {
+            q.push(curr->left);
+        }
+        if(curr->right==NULL)
+        {
+            curr->right=new node(val);
+            break;
+        }
+        else
+        {
+            q.push(curr->right);
+        }
+    }
+    return root;
+}
+void inorder(node* root)
+{
+    if(root==NULL)
+    {
+        cout<<"NULL";
+        return;
+    }
+    stack<node*>st;
+    node* curr=root;
+    while(!st.empty() ||curr!=NULL)
+    {
+        if(curr!=NULL)
+        {
+            st.push(curr);
+            curr=curr->left;
+        }
+        else
+        {
+            node* temp=st.top();
+            st.pop();
+            cout<<temp->data<<" ";
+            curr=temp->right;
+        }
+    }
+}
+int main()
+{
+    int n,val;
+    node* root=NULL;
+    cout<<"Enter the number of nodes: ";
+    cin>>n;
+    cout<<"Enter the nodes: ";
+    for(int i=0;i<n;i++)
+    {
+        cin>>val;
+        root=insert(root,val);
+    }
+    cout<<"Inorder Traversal is: ";
+    inorder(root);
+    return 0;
+}
