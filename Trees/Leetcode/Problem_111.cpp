4e73a5858eea20d509b5f98d283c02935c0c9974
Arjit Rawat <180564763+arjitrawat15@users.noreply.github.com>
2026-01-22 21:25:39 +0530

    Update and rename Problem 111 to Problem_111.cpp

---
+++ Trees/Leetcode/Problem_111.cpp
@@ -0,0 +1,27 @@
+# Minimum Depth of a Binary Tree
+## Given a binary tree, find its minimum depth. The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+
+int min_depth(node* root)
+{
+  if(root==NULL) return 0;
+  queue<node*>q;
+  q.push(root);
+  int depth=1;
+  while(!q.empty())
+  {
+    int size=q.size();
+    for(int i=0;i<size;i++)
+      {
+        node* curr=q.front();
+        q.pop();
+        if(curr->left==NULL && curr->right==NULL)
+        {
+            return depth;
+        }
+        if(curr->left) q.push(curr->left);
+        if(curr->right) q.push(curr->right);
+      }
+    depth++;
+  }
+  return depth;
+}
