ebc83eed292985f9aa82ad6e6fae31ce67762464
Arjit Rawat <180564763+arjitrawat15@users.noreply.github.com>
2026-01-26 18:57:46 +0530

    Create Leetcode_404.cpp

---
+++ Trees/Leetcode_404.cpp
@@ -0,0 +1,26 @@
+// Sum of Left Leaves
+// Given the root of a binary tree, return the sum of all left leaves.
+int sumOfLeftLeaves(node* root) 
+    {
+        queue<node*>q;
+        q.push(root);
+        int sum=0;
+        while(!q.empty())
+        {
+            node* curr=q.front();
+            q.pop();
+            if(curr->left!=NULL)
+            {
+                if(curr->left->left==NULL && curr->left->right==NULL)
+                {
+                    sum+=curr->left->val;
+                }
+                q.push(curr->left);
+            }
+            if(curr->right!=NULL)
+            {
+                q.push(curr->right);
+            }
+        }
+        return sum;
+    }
