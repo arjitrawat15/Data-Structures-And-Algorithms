ac660020845ff39092327d0ebbafafc415b4e978
Arjit Rawat <180564763+arjitrawat15@users.noreply.github.com>
2026-01-26 23:59:38 +0530

    Create Leetcode_572.cpp

---
+++ Trees/Leetcode_572.cpp
@@ -0,0 +1,25 @@
+// Subtree of another tree
+// Given the roots of two binary trees root and subRoot, return true if there is a subtree of root with the same structure and node values of subRoot and false otherwise.
+// Tc->O(
+bool isSubtree(node* root, node* subRoot) 
+    {
+        if(root==NULL)
+        {
+            return false;
+        }
+        if(root->val==subRoot->val && isSame(root,subRoot))
+        {
+            return true;
+        }
+        bool left=isSubtree(root->left,subRoot);
+        bool right=isSubtree(root->right,subRoot);
+        return left || right;
+    }
+    bool isSame(node* root,node* subRoot)
+    {
+        if(root==NULL || subRoot==NULL)
+        {
+            return (root==subRoot);
+        }
+        return root->val==subRoot->val && isSame(root->left,subRoot->left) && isSame(root->right,subRoot->right);
+    }
