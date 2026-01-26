fc71da587174f6befe1197af494444f2bd83b5c3
Arjit Rawat <180564763+arjitrawat15@users.noreply.github.com>
2026-01-26 18:58:10 +0530

    Update comments for minimum depth function

--- Trees/Leetcode_111.cpp
+++ Trees/Leetcode_111.cpp
@@ -1,5 +1,5 @@
-# Minimum Depth of a Binary Tree
-## Given a binary tree, find its minimum depth. The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+// Minimum Depth of a Binary Tree
+// Given a binary tree, find its minimum depth. The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
 // TC->O(N) SC->O(N)
 int min_depth(node* root)
 {
