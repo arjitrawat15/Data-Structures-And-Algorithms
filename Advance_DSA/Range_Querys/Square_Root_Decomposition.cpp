1717d42e2a3c35d5ed7ee3c60ec61181758d1ca4
Arjit Rawat <180564763+arjitrawat15@users.noreply.github.com>
2026-01-31 00:12:07 +0530

    Implement Square Root Decomposition for range queries
    Added functionality for handling multiple queries including updates and range sum calculations using Square Root Decomposition.

--- Advance_DSA/Range_Querys/Square_Root_Decomposition.cpp
+++ Advance_DSA/Range_Querys/Square_Root_Decomposition.cpp
@@ -1,12 +1,18 @@
+Square Root Decomposition 
+There are given N queries and for each query - 
+Option 1. Update the value at index i by given new value (1,index,val) is given 
+Option 2. Return the sum from l index to r index of the array.  (2,l,r) is given
+//Total time complexity -> O( Q x sqrt(N)) Space complexity -> O(sqrt(N))
+
 #include<bits/stdc++.h>
 using namespace std;
-void update(int index,int val,vector<int>& sqr,vector<int>& arr)
+void update(int index,int val,vector<int>& sqr,vector<int>& arr) // TC-> O(1)
 {
     int change=val-arr[index];
     sqr[index/sqr.size()]+=change;
     arr[index]=val;
 }
-int range_sum(int l,int r,vector<int>& sqr,vector<int>& arr)
+int range_sum(int l,int r,vector<int>& sqr,vector<int>& arr) // O(sqrt(N))
 {
     int blocks=sqr.size();
     int n=arr.size();
@@ -48,10 +54,10 @@ int range_sum(int l,int r,vector<int>& sqr,vector<int>& arr)
 int main()
 {
     int n;
-    cout<<"Enter the number of elements in array ";
+    cout<<"Enter the number of elements in array: ";
     cin>>n;
     vector<int>arr(n);
-    cout<<"Enter the array elements ";
+    cout<<"Enter the array elements: ";
     for(int i=0;i<n;i++)
     {
         cin>>arr[i];
@@ -62,7 +68,45 @@ int main()
     {
         sqr[i/blocks]+=arr[i];
     }
-    update(0,11,sqr,arr);
-    int ans=range_sum(0,6,sqr,arr);
-    cout<<ans;
+    int q;
+    cout<<"Enter the no of queries: ";
+    cin>>q;
+    for(int i=0;i<q;i++)
+    {
+        int ch;
+        cout<<"Enter the choice: ";
+        cin>>ch;
+        if(ch==1)
+        {
+            cout<<"Enter the index to be changed: ";
+            int index;
+            cin>>index;
+            cout<<"Enter the new value: ";
+            int val;
+            cin>>val;
+            update(0,11,sqr,arr);
+            cout<<"Value added successfully"<<endl;
+            cout<<"Array after updation is: ";
+            for(int i=0;i<n;i++)
+            {
+                cout<<arr[i]<<" ";
+            }
+            cout<<endl;
+        }
+        else if(ch==2)
+        {
+            int l,r;
+            cout<<"Enter the range: ";
+            cin>>l;
+            cin>>r;
+            int ans=range_sum(l,r,sqr,arr);
+            cout<<"Sum of the range is: "<<ans<<endl;
+        }
+        else
+        {
+            cout<<"Wrong choice !";
+            break;
+        }
+    }
+    return 0;
 }
