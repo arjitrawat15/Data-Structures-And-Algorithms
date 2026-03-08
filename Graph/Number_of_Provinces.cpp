48150ee49c8bbe01e53393ad22f5714b5ccd2d2b
Arjit Rawat <180564763+arjitrawat15@users.noreply.github.com>
2026-03-09 01:52:02 +0530

    Add files via upload

---
+++ Graph/Number_of_Provinces.cpp
@@ -0,0 +1,51 @@
+//To calculate the total no of provinces using DFS.
+//TC-> O(V + E) for dfs + O(V) ~ O(2N) 
+//SC->O(2V) ~ O(N) 
+#include<iostream>
+#include<vector>
+using namespace std;
+void dfs(int node,vector<int>& vis,vector<vector<int>>& adj)
+{
+    vis[node]=1;
+    for(auto it: adj[node])
+    {
+        if(!vis[it])
+        {
+            dfs(it,vis,adj);
+        }
+    }
+}
+int findProvinces(vector<vector<int>>& adj)
+{
+    int cnt=0;
+    int n=adj.size()-1;
+    vector<int>vis(n+1,0);
+    for(int i=1;i<=n;i++)
+    {
+        if(vis[i]==0)
+        {
+            dfs(i,vis,adj);
+            cnt++;
+        }
+    }
+    return cnt;
+}
+int main()
+{
+    int n,m;
+    cout<<"Enter the number of vertices: ";
+    cin>>n;
+    cout<<"Enter the number of edges: ";
+    cin>>m;
+    cout<<"Enter the edges: ";
+    vector<vector<int>>adj(n+1);
+    for(int i=0;i<m;i++)
+    {
+        int u,v;
+        cin>>u>>v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    cout<<"Number of Provinces are: "<<findProvinces(adj)<<endl;
+    return 0;
+}
