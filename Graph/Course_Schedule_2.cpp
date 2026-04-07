8ef7f914fdb07692cd44b04f61cc4c7d4a8ed4aa
Arjit Rawat <180564763+arjitrawat15@users.noreply.github.com>
2026-04-07 23:52:32 +0530

    Add Course Schedule 2 code using BFS

---
+++ Graph/Course_Schedule_2.cpp
@@ -0,0 +1,69 @@
+//To calculate the course schedule whether it is possible to finish all the courses or not if [ai, bi] are pre-requisties of 
+//doing course b before doing course a using Kahn's algorithm (BFS). 
+// Course Schedule 2-> If possible also return the order of finishing the courses. 
+// TC -> 2 X O(N + E) + 2 X O(N)
+// SC -> O(N) + O(N)
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+int main()
+{ 
+    int n,m;
+    cout<<"Enter the number of vertices: ";
+    cin>>n;
+    cout<<"Enter the number of edges: ";
+    cin>>m;
+    cout<<"Enter the edges: ";
+    vector<vector<int>>adj(n);
+    for(int i=0;i<m;i++)
+    {
+        int u,v;
+        cin>>u>>v;
+        adj[v].push_back(u); // [a,b] where b comes before a , so it is a reverse
+    }
+    vector<int> indegree(n,0);
+    queue<int> q;
+    for(int i=0;i<n;i++)
+    {
+        for(auto it:adj[i])
+        {
+            indegree[it]++;
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(indegree[i]==0)
+        {
+            q.push(i);
+        }
+    }
+    vector<int> topo;
+    while(!q.empty())
+    {
+        int node=q.front();
+        q.pop();
+        topo.push_back(node);
+        for(auto it:adj[node])
+        {
+            indegree[it]--;
+            if(indegree[it]==0)
+            {
+                q.push(it);
+            }
+        }
+    }
+    if(topo.size()==n)
+    {
+        cout<<"Topological sort is: ";
+        for(auto it:topo)
+        {
+            cout<<it<<" ";
+        }
+    }
+    else
+    {
+        cout<<"Not possible to finish all the courses"<<endl;
+    }
+    return 0;
+}
